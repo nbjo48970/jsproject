ebbbc8125eb1d842d15f6875bd8e8702b41b6eba
nbjo48970 <nbjo48970@edu.ucl.dk>
2023-02-22 14:11:05 +0100

    Update nyhedsbrev.js

--- nyhedsbrev.js
+++ nyhedsbrev.js
@@ -5,6 +5,11 @@ function ValidateEmail(input)
   let emailError = document.getElementById("email-error");
   let formInput = document.getElementById("email-input");
 
+
+if (inArray(input.value)) {
+  emailError.innerHTML = "Din e-mail er allerede registreret";
+  return false;
+}
   if (input.value.match(validRegex)){
     emailError.innerHTML = "Din e-mail er nu registreret";
     emailError.style.color = "green";
@@ -22,14 +27,10 @@ function ValidateEmail(input)
   }
 }
 
-// function RegisteredUsers(input){
-//
-//   let regUsers = ["pede@gmail.com", "thomas@mail.dk", "nicoline@mail.com"];
-//
-//   if (input.value.match(regUsers)){
-//     emailError.innerHTML = "ost";
-//     document.form1.text1.focus();
-//     return true;
-//   }
-//   return false;
-// }
+let regUsers = ["pede@gmail.com", "thomas@mail.dk", "nicoline@mail.com"];
+function inArray(inVal){
+    for( var i=0, len=regUsers.length; i < len; i++){
+        if (regUsers[i] == inVal) return true;
+    }
+    return false;
+}
