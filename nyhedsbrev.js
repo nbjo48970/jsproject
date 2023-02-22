2cb83a046d98486b9f26e1083d4eb2483d0d960b
nbjo48970 <nbjo48970@edu.ucl.dk>
2023-02-22 11:15:26 +0100

    nlkæl

--- nyhedsbrev.js
+++ nyhedsbrev.js
@@ -1,14 +1,17 @@
 function ValidateEmail(input) {
 
   let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let emailError = document.getElementById("email-error");
 
   if (input.value.match(validRegex)){
-    alert("Valid email address!");
+    emailError.innerHTML = "Din e-mail er nu registreret";
+    emailError.style.color = "green";
     document.form1.text1.focus();
-    return true;
+    return false;
   }
     else {
-    alert("Invalid email address!");
+      emailError.innerHTML = "Indtast gyldig e-mailadresse";
+      emailError.style.color = "red";
     document.form1.text1.focus();
     return false;
   }
