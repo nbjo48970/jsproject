ddc2686791b5062c58780554164e695c6fa7f947
DrWilliz <xander_1999@yahoo.dk>
2023-02-23 00:13:57 +0100

    dropdownBULL

--- BWAKH.js
+++ BWAKH.js
@@ -5,3 +5,26 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+function myDrop() {
+  document.getElementById("myDropdown").classList.toggle("lookatmenow");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('lookatmenow')) {
+        openDropdown.classList.remove('lookatmenow');
+      }
+    }
+  }
+}
+
+var help = ["Produkter", "Brands", "FAQ", "Fragt", "Smiley-ordning"];
+
+function helpMe()  {
+  var p = document.getElementById
+}
