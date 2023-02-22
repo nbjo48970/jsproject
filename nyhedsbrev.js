c3566e4b01ff4fd21e05b4119d8f91a53d3f0c91
nbjo48970 <nbjo48970@edu.ucl.dk>
2023-02-22 10:02:49 +0100

    cghjk

--- nyhedsbrev.js
+++ nyhedsbrev.js
@@ -1,1 +1,16 @@
-console.log("hej");
+function ValidateEmail(input) {
+
+  let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.value.match(validRegex)){
+    alert("Valid email address!");
+    document.form1.text1.focus();
+    return true;
+  }
+    else {
+    alert("Invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
+
+}
