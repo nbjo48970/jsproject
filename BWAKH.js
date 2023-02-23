510141628397c03a7177c63c69798ad623823148
DrWilliz <xander_1999@yahoo.dk>
2023-02-23 12:55:01 +0100

    killme

--- BWAKH.js
+++ BWAKH.js
@@ -1,3 +1,5 @@
+document.getElementById("chat-button").addEventListener("click", openForm, false);
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
@@ -23,8 +25,69 @@ window.onclick = function(event) {
   }
 }
 
-var help = ["Produkter", "Brands", "FAQ", "Fragt", "Smiley-ordning"];
+document.getElementById("send-it").addEventListener("click", watchIt, false);
 
-function helpMe()  {
-  var p = document.getElementById
+function watchIt(textaera) {
+
+
+
+if (inArray(document.getElementById("support").value)) {
+  alert("Hov, hov du")
 }
+
+else {
+  alert("Vi har modtaget din besked!");
+}
+
+}
+
+let language = ["pattesutter", "Projekt 2", "Mette Frederiksen", "Niko", "Schmeter"];
+function inArray(inVal){
+    for( var i=0, len=language.length; i < len; i++){
+        if (language[i] == inVal) return true;
+    }
+    return false;
+}
+
+
+
+// document.getElementById("product-button").addEventListener("click", produktBwakh, false);
+//
+// function produktBwakh() {
+//   let getIt = 0;
+// }
+//
+// document.getElementById("brands-button").addEventListener("click", brandsBwakh, false);
+//
+// function brandsBwakh() {
+//   var getIt = 1;
+// }
+//
+// document.getElementById("FAQ-button").addEventListener("click", faqBwakh, false);
+//
+// function faqBwakh() {
+//   var getIt = 2;
+// }
+//
+// document.getElementById("fragt-button").addEventListener("click", fragtBwakh, false);
+//
+// function fragtBwakh() {
+//   var getIt = 3;
+// }
+//
+// document.getElementById("smiley-button").addEventListener("click", smileyBwakh, false);
+//
+// function smileyBwakh() {
+//   var getIt = 4;
+// }
+//
+// document.querySelector("button").addEventListener("click", helpMe, false);
+//
+// function helpMe()  {
+//   if (getIt = 0) {
+//     console.log (help[0])
+//   }
+//   else {
+//     console.log ("FAWK");
+// }
+// }
