39fe2e3679757684c6eb0c2e49e2b705153397e1
PeterSogaard98 <119843293+PeterSogaard98@users.noreply.github.com>
2023-02-25 22:52:08 +0100

    lårt

--- peter.js
+++ peter.js
@@ -1,7 +1,7 @@
 function slideUp(element){
   let elem = document.getElementById(element);
-  elem.style.transition = 'height 0.5s linear 0s';
-  elem.style.height = '100px';
+  elem.style.transition = 'height 0.5s ease-in 0s';
+  elem.style.height = '50px';
 }
 
 function slideDown(element){
