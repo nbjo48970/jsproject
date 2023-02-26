c390b68e0af3e66d524dc7b30077e3429d17c256
Macucous <macucous@gmail.com>
2023-02-26 16:26:09 +0100

    footer array update

--- KMS.js
+++ KMS.js
@@ -6,4 +6,11 @@ function closeNav() {
     document.getElementById("con-portal").style.width = "0";
   }
 
-console.log("hi")
+// console.log("hi")
+
+let countryArray = ["Sweden", "Finland", "Norway", "Denmark"]
+let countryPick = document.getElementById("country-pick")
+
+for (let i = 0; i < countryArray.length; i++) {
+    countryPick.textContent = countryArray[i]
+}
