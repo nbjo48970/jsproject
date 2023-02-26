e525de8ada20b0b618bde85c3bfff869a4dab5c7
PeterSogaard98 <119843293+PeterSogaard98@users.noreply.github.com>
2023-02-26 20:46:30 +0100

    lårt done

--- peter.js
+++ peter.js
@@ -1,11 +1,76 @@
+let elementHeights = ['50px', '0px'];
+
 function slideUp(element){
-  let elem = document.getElementById(element);
-  elem.style.transition = 'height 0.5s ease-in 0s';
-  elem.style.height = '50px';
+  let mainText = document.getElementById(element);
+  mainText.style.transition = 'height 0.5s ease-in 0s';
+  mainText.style.height = elementHeights[0];
+
+  if (mainText.style.height == elementHeights[0]) {
+    let hiddenText = document.getElementById('hidden-txt1');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 1;
+  }
+
+  if (mainText.style.height == elementHeights[0]) {
+    let hiddenText = document.getElementById('hidden-txt2');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 1;
+  }
+
+  if (mainText.style.height == elementHeights[0]) {
+    let hiddenText = document.getElementById('hidden-txt3');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 1;
+  }
+
+  if (mainText.style.height == elementHeights[0]) {
+    let hiddenText = document.getElementById('hidden-txt4');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 1;
+  }
+
+  else {
+    hiddenText.style.opacity = 0;
+    console.log('peter')
+  }
 }
 
 function slideDown(element){
-  let elem = document.getElementById(element);
-  elem.style.transition = 'height 0.5s linear 0s';
-  elem.style.height = '0px';
+  let mainText = document.getElementById(element);
+  mainText.style.transition = 'height 0.5s ease-in 0s';
+  mainText.style.height = elementHeights[1];
+
+  if (mainText.style.height == elementHeights[1]) {
+    console.log('peter2')
+    let hiddenText = document.getElementById('hidden-txt1');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 0;
+  }
+
+  if (mainText.style.height == elementHeights[1]) {
+    console.log('peter2')
+    let hiddenText = document.getElementById('hidden-txt2');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 0;
+  }
+
+  if (mainText.style.height == elementHeights[1]) {
+    console.log('peter2')
+    let hiddenText = document.getElementById('hidden-txt3');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 0;
+  }
+
+  if (mainText.style.height == elementHeights[1]) {
+    console.log('peter2')
+    let hiddenText = document.getElementById('hidden-txt4');
+    hiddenText.style.transition = 'opacity 0.5s linear 0s'
+    hiddenText.style.opacity = 0;
+  }
+
+  else {
+    let hiddenText = document.getElementById('hidden-txt1');
+    hiddenText.style.opacity = 0;
+    console.log('peter')
+  }
 }
